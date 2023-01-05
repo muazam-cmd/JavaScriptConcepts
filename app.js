2e009e6a8cd14fdd2a71a68eec051885c57841c8
muazam-cmd <muazamshahid7@gmail.com>
2023-01-05 13:51:21 +0400

    types and operators

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-// long running function
+// // long running function
 
-function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms) { }
-    console.log('Finished Function');
-}
+// function waitThreeSeconds() {
+//     var ms = 3000 + new Date().getTime();
+//     while (new Date() < ms) { }
+//     console.log('Finished Function');
+// }
 
-function clickHandler() {
-    console.log('click event');
-}
+// function clickHandler() {
+//     console.log('click event');
+// }
 
-// listen for the click event
+// // listen for the click event
 
-document.addEventListener('click', clickHandler);
+// document.addEventListener('click', clickHandler);
 
-waitThreeSeconds();
-console.log('Finished execution!')
+// waitThreeSeconds();
+// console.log('Finished execution!')
+
+console.log(libraryName);
